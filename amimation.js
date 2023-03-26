7684df093862057979de424beae2d6d2afb66dd1
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2023-03-26 22:38:07 +0530

    deleted

--- amimation.js
+++
@@ -1,2 +0,0 @@
-This is animation JS
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus porro veritatis quae similique enim, eos earum animi quibusdam fuga nesciunt maxime velit laboriosam perferendis, incidunt voluptate aperiam aliquam natus saepe.
